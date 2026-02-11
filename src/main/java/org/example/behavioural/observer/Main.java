a4d280a1a5746f9def991e38f86333bc5735be7b
Sangili <sangilimurugan492@gmail.com>
2026-02-11 22:41:50 +0530

    1. Observer Pattern Example added main class for testing

---
+++ src/main/java/org/example/behavioural/observer/Main.java
@@ -0,0 +1,10 @@
+package org.example.behavioural.observer;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Exercise exercise = new Exercise();
+        exercise.run();
+
+    }
+}
